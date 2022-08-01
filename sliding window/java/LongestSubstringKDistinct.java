1936b4fbce45f6de2217194108ceed37cb358487
Joyce <adelusi@troveapp.co>
2022-08-01 16:33:30 +0100

    longest substring with no more than k distinct java

---
+++ sliding window/java/LongestSubstringKDistinct.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+public class LongestSubstringKDistinct {
+
+    public static void main(String[] args) {
+        
+        System.out.println(findLength("araaci", 2));
+        System.out.println(findLength("araaci", 1));
+        System.out.println(findLength("cbbebi", 3));
+
+    }
+    
+    public static int findLength(String str, int k) {
+
+        HashMap<Character, Integer> characterLength = new HashMap<>();
+        int windowStart = 0;
+        int result = Integer.MIN_VALUE;
+        int windowLength = 0;
+        for(int windowEnd = 0; windowEnd < str.length(); windowEnd++){
+          char c = str.charAt(windowEnd);
+          windowLength++;
+          characterLength.put(c, characterLength.getOrDefault(c, 0) + 1);
+    
+          while(characterLength.size() > k){
+            char ch = str.charAt(windowStart);
+            characterLength.put(ch, characterLength.get(ch) - 1);
+    
+            if(characterLength.get(ch) <= 0){
+              characterLength.remove(ch);
+            }
+    
+            windowStart++;
+            windowLength--;
+    
+          }
+          result = Math.max(result, windowLength);
+        }
+    
+        return result;
+      }
+
+}
