a84a6b6213a32b9698b4aa34bffab324be6dd58a
Joyce-Adelusi <joyce@vendy.money>
2023-01-17 21:30:20 +0100

    AverageSubArrayOfSizeK tsc

---
+++ sliding window/typescript/AverageSubArrayOfSizeK.ts
@@ -0,0 +1,21 @@
+//function to find the average of numbers in all sub-arrays of size k
+const AverageSubArrayOfSizeK = (k: number, array: number[]) => {
+    let windowStart = 0;
+    let windowSum = 0;
+    let result: number[] = [];
+
+    for (let windowEnd = 0; windowEnd < array.length; windowEnd++) {
+        windowSum += array[windowEnd];
+
+        if (windowEnd >= k - 1) {
+            result.push(windowSum / k);
+            windowSum -= array[windowStart];
+            windowStart++;
+        }
+    }
+
+    return result;
+
+};
+
+console.log(AverageSubArrayOfSizeK(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]));
